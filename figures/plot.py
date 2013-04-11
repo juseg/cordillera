2550181d43f11541a2109f169fef2c6077736bbc
Julien Seguinot <julien.seguinot@natgeo.su.se>
2013-04-11 22:27:57 +0200

    Add plotting function for 'best' runs
    The 'best' temperature offset values still need to be updated.

--- figures/plot.py
+++ figures/plot.py
@@ -5,41 +5,70 @@ from netCDF4 import Dataset
 from matplotlib import pyplot as plt
 from iceplot import plot as iplt
 
-# select input data
-climates = ['wcnn', 'erai', 'narr', 'cfsrs7', 'cfsr', 'ncar']
-labels   = ['WorldClim', 'ERA-Interim', 'NARR', 'smoothed CFSR', 'CFSR', 'NCEP/NCAR']
+### Globals ###
 
-# given temperature offset
+labels = {
+  'cfsr':   'CFSR',
+  'cfsrs7': 'smoothed CFSR',
+  'erai':   'ERA-Interim',
+  'narr':   'NARR',
+  'ncar':   'NCEP/NCAR',
+  'wc':     'WorldClim',
+  'wcnn':   'WorldClim'}
+
+### Results plotting functions ###
+
+def _plot_results(climates, offsets, title, output):
+    """Base function to plot results"""
+
+    # initialize figure
+    fig = iplt.gridfigure((30., 60.), (2, len(climates)/2), cbar_mode='single')
+
+    # loop on climate datasets
+    for i, clim in enumerate(climates):
+
+      # plot
+      nc = Dataset('../data/%s-%s.nc' % (clim, offsets[i]))
+      ax = plt.axes(fig.grid[i])
+      im = iplt.icemap(nc)
+      ax.set_title(title % {'label': labels[clim], 'dt': offsets[i]})
+
+      # colorbar
+      cb = fig.colorbar(im, fig.grid.cbar_axes[0])
+      cb.set_label('ice surface velocity (m/yr)')
+
+    # save
+    fig.savefig(output + '.png')
+    fig.savefig(output + '.pdf')
+
+def best():
+    """Plot icemaps for the best runs"""
+    _plot_results(
+      climates = ['wcnn', 'erai', 'narr', 'cfsrs7', 'cfsr', 'ncar'],
+      offsets  = ['09', '07', '08', '05', '04', '05'],
+      title    = '%(label)s - %(dt)s K',
+      output   = 'cordillera-climate-best')
+
 def cool(cool):
-  	fig = iplt.gridfigure((30., 60.), (2, len(climates)/2), cbar_mode='single')
-  	for i in range(len(climates)):
-  		clim  = climates[i]
-  		label = labels[i]
-  
-  		# plot
-  		print 'drawing %s cool%s...' % (clim, cool)
-  		nc = Dataset('../data/%s-%s.nc' % (clim, cool))
-  		ax = plt.axes(fig.grid[i])
-  		im = iplt.icemap(nc)
-  		ax.set_title(label)
-  
-  		# colorbar
-  		cb = fig.colorbar(im, fig.grid.cbar_axes[0])
-  		cb.set_label('ice surface velocity (m/yr)')
-  
-  	# save
-  	output = 'cordillera-climate-cool%s' % cool
-  	fig.savefig(output + '.png')
-  	fig.savefig(output + '.pdf')
+    """Plot icemaps for given temperature offset"""
+    _plot_results(
+      climates = ['wcnn', 'erai', 'narr', 'cfsrs7', 'cfsr', 'ncar'],
+      offsets  = [cool]*6,
+      title    = '%(label)s',
+      output   = 'cordillera-climate-cool' + cool)
 
+### Command-line interface ###
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--cool',
       help='plot icemaps for given temperature offset')
+    parser.add_argument('--best', action='store_true',
+      help='plot icemaps for so-called best runs')
     args = parser.parse_args()
 
     if args.cool is not None: cool(args.cool)
+    if args.best is not None: best()
 
 
