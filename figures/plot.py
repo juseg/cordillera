96187570f6af67cacfd4bb86e1f76ea9ea88ff3f
Julien Seguinot <julien.seguinot@natgeo.su.se>
2013-04-12 12:11:57 +0200

    Add plotting function for ice extent

--- figures/plot.py
+++ figures/plot.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import numpy as np
 from netCDF4 import Dataset
 from matplotlib import pyplot as plt
 from iceplot import plot as iplt
 
 ### Globals ###
 
+mapsize = (30., 60.)
 labels = {
   'cfsr':   'CFSR',
   'cfsrs7': 'smoothed CFSR',
@@ -16,34 +18,63 @@ labels = {
   'wc':     'WorldClim',
   'wcnn':   'WorldClim'}
 
-### Results plotting functions ###
+### Base function ###
 
-def _plot_results(climates, offsets, title, output):
-    """Base function to plot results"""
+def _plot_maps(climates, clabel, output, func, cticks=None):
+    """Base function to plot maps"""
 
     # initialize figure
-    fig = iplt.gridfigure((30., 60.), (2, len(climates)/2), cbar_mode='single')
+    fig = iplt.gridfigure(mapsize, (2, len(climates)/2), cbar_mode='single')
 
     # loop on climate datasets
     for i, clim in enumerate(climates):
-
-      # plot
-      nc = Dataset('../data/%s-%s.nc' % (clim, offsets[i]))
       ax = plt.axes(fig.grid[i])
-      im = iplt.icemap(nc)
-      ax.set_title(title % {'label': labels[clim], 'dt': offsets[i]})
+      sm = func(i, clim)
 
-      # colorbar
-      cb = fig.colorbar(im, fig.grid.cbar_axes[0])
-      cb.set_label('ice surface velocity (m/yr)')
+    # add colorbar
+    cb = fig.colorbar(sm, fig.grid.cbar_axes[0], ticks=cticks)
+    cb.set_label(clabel)
 
     # save
+    print 'saving ' + output
     fig.savefig(output + '.png')
     fig.savefig(output + '.pdf')
 
+### Results plotting functions ###
+
+def _plot_icemaps(climates, offsets, title, output):
+    """Base function to plot icemaps"""
+
+    def func(i, clim):
+      nc = Dataset('../data/%s-%s.nc' % (clim, offsets[i]))
+      plt.title(title % {'label': labels[clim], 'dt': offsets[i]})
+      return iplt.icemap(nc)
+
+    _plot_maps(climates, 'ice surface velocity (m/s)', output, func)
+
+def _plot_extents(climates, title, output):
+    """Base functiont to plot ice extents"""
+
+    def func(i, clim):
+      icecover = 10
+      for dt in range(9,-1,-1):
+        nc = Dataset('../data/%s-%02g.nc' % (clim, dt))
+        thkmask = nc.variables['thk'][0] > 1
+        icecover = np.where(nc.variables['thk'][0] > 1, dt, icecover)
+      plt.title(title % {'label': labels[clim]})
+      #cs = plt.contour(icecover.T,
+        #levels = range(0, 10),
+        #colors = 'black',
+        #linewidths = 0.2)
+      return plt.contourf(icecover.T,
+        levels = range(-1, 10),
+        cmap   = plt.cm.Blues_r)
+
+    _plot_maps(climates, u'temperature offset (°C)', output, func, range(10))
+
 def best():
     """Plot icemaps for the best runs"""
-    _plot_results(
+    _plot_icemaps(
       climates = ['wcnn', 'erai', 'narr', 'cfsrs7', 'cfsr', 'ncar'],
       offsets  = ['09', '07', '08', '05', '04', '05'],
       title    = '%(label)s - %(dt)s K',
@@ -51,11 +82,18 @@ def best():
 
 def cool(cool):
     """Plot icemaps for given temperature offset"""
-    _plot_results(
+    _plot_icemaps(
       climates = ['wcnn', 'erai', 'narr', 'cfsrs7', 'cfsr', 'ncar'],
       offsets  = [cool]*6,
       title    = '%(label)s',
       output   = 'cordillera-climate-cool' + cool)
+
+def extent():
+    """Plot stacked ice extents"""
+    _plot_extents(
+      climates = ['wcnn', 'erai', 'narr', 'cfsrs7', 'cfsr', 'ncar'],
+      title    = '%(label)s',
+      output   = 'cordillera-climate-extent')
 
 ### Command-line interface ###
 
@@ -66,9 +104,12 @@ if __name__ == "__main__":
       help='plot icemaps for given temperature offset')
     parser.add_argument('--best', action='store_true',
       help='plot icemaps for so-called best runs')
+    parser.add_argument('--extent', action='store_true',
+      help='plot stacked ice extent from all runs')
     args = parser.parse_args()
 
     if args.cool is not None: cool(args.cool)
-    if args.best is not None: best()
+    if args.best is True: best()
+    if args.extent is True: extent()
 
 
