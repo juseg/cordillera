b61debfb4fefb9e6d1b28963cd0fdb798373d566
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-06-05 11:50:06 +0200

    Add warm-base duration plot.

---
+++ figures/warmbase.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import numpy as np
+from netCDF4 import Dataset
+from matplotlib import pyplot as plt
+from matplotlib.colors import BoundaryNorm, LogNorm
+from iceplot import plot as iplt
+from paperglobals import *
+
+# simulations used
+res = '10km'
+records = ['grip', 'epica']
+offsets = [5.8, 5.6]
+
+# initialize figure
+fig = iplt.gridfigure((47.5, 95.0), (1, len(records)), axes_pad=2.5*in2mm,
+                      cbar_mode='single', cbar_pad=2.5*in2mm, cbar_size=5*in2mm)
+
+# draw topo and coastline
+draw_boot_topo(fig.grid, res)
+
+# loop on records[i]
+for i, rec in enumerate(records):
+    print 'reading %s extra output...' % rec
+    ax = fig.grid[i]
+    this_run_path = run_path % (res, rec, offsets[i]*100)
+
+    # read extra output
+    nc = Dataset(this_run_path + '-extra.nc')
+    x = nc.variables['x']
+    y = nc.variables['y']
+    w = (3*x[0]-x[1])/2
+    e = (3*x[-1]-x[-2])/2 - (x[-1]-x[-2])/2  # weird but works
+    n = (3*y[0]-y[1])/2
+    s = (3*y[-1]-y[-2])/2 - (y[-1]-y[-2])/2  # weird but works
+    mask = nc.variables['mask']
+    temp = nc.variables['temppabase']
+    
+    # compute duration of warm-based coved
+    warm = (temp[:] > -1e-9) * (mask[:] == 2)
+    warm = np.ma.array((temp[:] > -1e-9), mask=(mask[:] != 2))
+    warm = warm.sum(axis=0).T/10.0
+
+    # set levels, colors and hatches
+    levs = [-1] + range(0, 121, 20)
+    cmap = plt.get_cmap('Reds')
+    colors = cmap(np.linspace(0.0, 1.0, len(levs)-1))
+    hatches = ['//'] + ['']*(len(levs)-2)
+
+    # draw contours
+    cs = ax.contourf(x[:], y[:], warm, levels=levs, alpha=0.75,
+                     colors=colors, hatches=hatches)
+    ax.contour(x[:], y[:], warm, [0.0], colors='k', linewidths=0.25)
+    ax.contour(x[:], y[:], warm.mask, [0.5], colors='k', linewidths=0.5)
+
+    # close extra file
+    annotate(ax, rec.upper())
+    nc.close()
+
+# add colorbar and save
+cb = fig.colorbar(cs, ax.cax, ticks=levs[1:])
+cb.set_label('Duration of warm-based ice cover')
+fig.savefig('warmbase.png')
