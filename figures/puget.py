e0e1e3a59171b6490e966b96ee2076b4fa4ba1a1
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-06-10 20:58:16 +0200

    Update Puget plot script.

--- figures/puget.py
+++ figures/puget.py
@@ -11,30 +11,34 @@ from iceplot import plot as iplt
 from paperglobals import *
 
 # initialize figure
-times = range(949, 1030, 10)
-imin, imax = 25, 74
-jmin, jmax = 10, 59
-fig = iplt.gridfigure((25.0, 25.0), (3, 3), axes_pad=2.5*in2mm,
+times = np.arange(-23.0, -17.0, 0.5)
+imin, imax = 60, 99
+jmin, jmax = 30, 69
+fig = iplt.gridfigure((25.0, 25.0), (4, 3), axes_pad=2.5*in2mm,
                       cbar_mode='none', cbar_location='bottom',
                       cbar_pad=2.5*in2mm, cbar_size=5*in2mm)
+
+# load extra output
+print 'reading extra output...'
+this_run_path = run_path % ('6km', 'grip', 580)
+nc = Dataset(this_run_path + '-extra.nc')
 
 # loop on records[i]
 for i, t in enumerate(times):
 
-    # load extra output
-    print 'reading extra output...'
-    this_run_path = run_path % ('10km', 'grip', 580)
-    nc = Dataset(this_run_path + '-extra.nc')
-    time = nc.variables['time'][t]*s2ka
-    mask = nc.variables['mask'][t,imin:imax,jmin:jmax].T
-    topg = nc.variables['topg'][t,imin:imax,jmin:jmax].T
-    usurf = nc.variables['usurf'][t,imin:imax,jmin:jmax].T
-    u = nc.variables['uvelsurf'][t,imin:imax,jmin:jmax].T
-    v = nc.variables['vvelsurf'][t,imin:imax,jmin:jmax].T
-    thk = nc.variables['thk'][t,imin:imax,jmin:jmax].T
-    nc.close()
+    # find nearest time slice
+    k = np.argmin(np.abs(nc.variables['time'][:]*s2ka-t))
 
-    # post-processing
+    # slice
+    time = nc.variables['time'][k]*s2ka
+    mask = nc.variables['mask'][k,imin:imax,jmin:jmax].T
+    topg = nc.variables['topg'][k,imin:imax,jmin:jmax].T
+    usurf = nc.variables['usurf'][k,imin:imax,jmin:jmax].T
+    u = nc.variables['uvelsurf'][k,imin:imax,jmin:jmax].T
+    v = nc.variables['vvelsurf'][k,imin:imax,jmin:jmax].T
+    thk = nc.variables['thk'][k,imin:imax,jmin:jmax].T
+
+    # apply masks
     icy = (mask == 2)
     usurf = np.ma.array(usurf, mask=(1-icy))
     u = np.sign(u)*np.log(1+np.abs(u)/100)
@@ -42,18 +46,20 @@ for i, t in enumerate(times):
     c = (u**2 + v**2)**0.5
 
     # plot
+    print 'plotting at %s kyr...' % time
     ax = fig.grid[i]
     ax.imshow(topg-125.0, cmap=topo_cmap, norm=topo_norm)
     ax.contour(usurf, levels=range(100, 5000, 100), colors='k', linewidths=0.2)
     ax.contour(usurf, levels=range(1000, 5000, 1000), colors='k', linewidths=0.5)
     ax.contourf(icy, levels=[0.5, 1.5], colors='w', alpha=0.75)
     ax.contour(icy, levels=[0.5], colors='k')
-    ax.quiver(u, v, c, cmap=vel_cmap)
+    ax.quiver(u, v, c, cmap=vel_cmap, scale=25.0)
     annotate(ax, '%s kyr' % (time))
 
 # add colorbar and save
 #cb = fig.colorbar(cs, ax.cax, ticks=levs[::2],
 #                  format=FuncFormatter(lambda x, pos: '%g' % (x/1000.0)))
 #cb.set_label(r'surface elevation (km)')  #, labelpad=-1.5*pt2mm)
-print 'saving snapshots...'
+print 'saving...'
 fig.savefig('puget')
+nc.close()
