1c232252c47bef5f72445966ead009bb42052302
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-06-23 10:32:46 +0200

    Add iceplot as a submodule.

--- figures/lgmquiver.py
+++ figures/lgmquiver.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python2
 # coding: utf-8
 
+import sys
+
+sys.path.append('iceplot')
+
 import numpy as np
 from netCDF4 import Dataset
 from matplotlib import pyplot as mplt
 from iceplot import plot as iplt
+
+print iplt
 
 # unit conversion
 mm2in = 1/25.4
 s2ka = 1/(365.0 * 24 * 60 * 60 * 1000)
 
 # file path
-run_path = '/home/julien/pism/output/cordillera-narr-5km-bl/' \
-           'grip3222cool580+ccyc+till1545/y0120000'
+run_path = '/home/julien/pism/output/cordillera-narr-6km-bl/' \
+           'epica3222cool560+ccyc+till1545/y0120000'
 
 # use time series to locate glacial maximum
 nc  = Dataset(run_path + '-ts.nc')
@@ -24,7 +30,7 @@ nc.close()
 print lgm_indx, lgm_time*s2ka
 
 # round lgm time to nearest slice
-nc  = Dataset(run_path + '-extra--0021700.000.nc')
+nc  = Dataset(run_path + '-extra.nc')
 time = nc.variables['time']  #*s2ka
 lgm_indx = (np.abs(time[:]-lgm_time)).argmin()
 lgm_time = time[lgm_indx]
@@ -36,16 +42,18 @@ ax  = fig.add_axes([0, 0, 1, 1])
 ax.axis('off')
 
 # bed topography and ice margin
-im = iplt.bedtopoimage(nc, lgm_indx)
-cs = iplt.icemargincontour(nc, lgm_indx, colors='white', alpha=0.5)
+im = iplt.imshow(nc, 'topg', lgm_indx)
+cs = iplt.icemargin(nc, lgm_indx)
 
 # velocity quiver
-qv = iplt.surfvelquiver(nc, lgm_indx, width=0.0005) #, scale = 250
+qv = iplt.quiver(nc, 'velsurf', lgm_indx, width=0.0005) #, scale = 250
 qv.set_rasterized(True)
 
 # surface topography contours
-cs = iplt.surftopocontour(nc, lgm_indx, levels=range(100, 6000, 100), linewidths=0.1)
-cs = iplt.surftopocontour(nc, lgm_indx, levels=range(1000,6000,1000), linewidths=0.5)
+cs = iplt.contour(nc, 'usurf', lgm_indx, levels=range(100, 6000, 100),
+                  cmap=None, colors='k', linewidths=0.1)
+cs = iplt.contour(nc, 'usurf', lgm_indx, levels=range(1000,6000,1000),
+                  cmap=None, colors='k', linewidths=0.5)
 cs.clabel(fontsize=6, fmt='%g')
 
 # colorbars
