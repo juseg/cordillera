7e3092ffc1859514383c65b0ea1c89dfeda031c8
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-06-30 07:52:41 +0200

    Improve paleo-glaciation plot.

--- figures/data/get_quatglac.sh
+++ figures/data/get_quatglac.sh
@@ -3,7 +3,7 @@
 url="http://static.us.elsevierhealth.com/ehlers_digital_maps/\
 digital_maps_02_all_other_files.zip"
 archive=$(basename $url)
-input=lgm_global.shp
+input=lgm.shp
 output=etopo1.nc
 
 # download archive
@@ -11,11 +11,8 @@ output=etopo1.nc
 
 # inflate archive
 # (we need to escape the wildcards to not match already inflated files)
-[ -f $input ] || unzip -u $archive ${input%.shp}.\* LGM_global.dbf
-
-# correct naming bug
-[ -f ${input%.shp}.dbf ] || ln -s LGM_global.dbf ${input%.shp}.dbf
+[ -f $input ] || unzip -u $archive ${input%.shp}.\*
 
 # remove small polygons and simplify others
 ogr2ogr ${input%.shp}_simple.shp $input -overwrite \
-    -where "OGR_GEOM_AREA > 1e-3" -simplify 0.1    
+    -where "OGR_GEOM_AREA > 1e-3" -simplify 0.1
--- figures/paleo-glaciation.py
+++ figures/paleo-glaciation.py
@@ -12,23 +12,30 @@ bwu = 0.5  # base width unit
 scale = '50m'
 
 # Projection
-proj = ccrs.NorthPolarStereo()
+proj = ccrs.Orthographic(central_longitude=-90.0, central_latitude=75.0)
 
-# Initialize figure
-fig = plt.figure(0, (80/25.4, 80/25.4))
-ax = fig.add_axes([0, 0, 1, 1], projection=proj)
-ax.set_xlim(-6e6, 6e6)
-ax.set_ylim(-6e6, 6e6)
+# Initialize figure in inches
+figw, figh = 80.0/25.4, 40.0/25.4
+fig = plt.figure(0, (figw, figh))
+fig.subplots_adjust(left=0.5/72/figw, bottom=0.5/72/figh,
+                    right=1-0.5/72/figw, top=1-0.5/72/figh,
+                    wspace=1/((1+figw/(1./72))/2-1))
+grid = [fig.add_subplot(121+i, projection=proj) for i in range(2)]
 
-# add NaturalEarth coastlines
-ax.add_feature(cfeature.NaturalEarthFeature(
-    category='physical', name='coastline', scale=scale,
-    edgecolor='k', facecolor='none', lw=0.5*bwu))
+# draw common parts
+for ax in grid:
+    ax.gridlines(color='0.5', linestyle='-', linewidth=0.1)
+    ax.coastlines(edgecolor='k', lw=0.25)
+
+# add present glaciers
+grid[0].add_feature(cfeature.NaturalEarthFeature(
+    category='physical', name='glaciated_areas', scale=scale,
+    edgecolor='none', facecolor='#0978ab', alpha=0.5,))
 
 # add Ehlers & Gibbard 2003 LGM outline
 # try to identify duplicates using centroids
 centroids = []
-shp = shpreader.Reader('data/lgm_global_simple.shp')
+shp = shpreader.Reader('data/lgm_simple.shp')
 for i, geom in enumerate(shp.geometries()):
     x, y = geom.centroid.xy
     x, y = x[0], y[0]
@@ -41,13 +48,8 @@ for i, geom in enumerate(shp.geometries()):
     else:
         print 'adding record %i with area %f ...' % (i, geom.area)
         centroids.append((x, y))
-        ax.add_geometries(geom, ccrs.PlateCarree(), alpha=0.5,
-                  edgecolor='none', facecolor='#0978ab', lw=bwu)
-
-# add graticules
-#ax.add_feature(cfeature.NaturalEarthFeature(
-#    category='physical', name='graticules_15', scale=scale,
-#    edgecolor='#000000', facecolor='none', lw=0.3*bwu))
+        grid[1].add_geometries(geom, ccrs.PlateCarree(),
+                  edgecolor='none', facecolor='#0978ab', alpha=0.5)
 
 # save
 fig.savefig('paleo-glaciation', dpi=254)
