8b012beb1b0a3d032d4cae50d25a3270939825de
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-07-02 16:15:37 +0200

    Add preliminary map of the Cordillera.

--- figures/map-cordillera.py
+++ figures/map-cordillera.py
@@ -18,20 +18,25 @@ pt2mm = 72*in2mm
 bwu = 0.5  # base width unit
 scale = '50m'
 
-# Canadian Atlas Lambert projection
-proj = ccrs.LambertConformal(
+# Geographic projection
+cal = ccrs.LambertConformal(
     central_longitude=-95.0, central_latitude=49.0,
     false_easting=0.0, false_northing=0.0,
     secant_latitudes=(49.0, 77.0), globe=None, cutoff=0)
+proj = ccrs.LambertConformal(
+    central_longitude=-135.0, central_latitude=45.0,
+    false_easting=0.0, false_northing=0.0,
+    secant_latitudes=(45.0, 70.0), globe=None, cutoff=0)
 
 # Initialize figure
-fig = plt.figure(0, (170/25.4, 100/25.4))
+fig = plt.figure(0, (170/25.4, 120/25.4))
 ax = fig.add_axes([0.0, 0.0, 1.0, 1.0], projection=proj)
-ax.set_xlim((-4250e3, 4250e3))
-ax.set_ylim((-1200e3, 3800e3))
+ax.set_xlim((-2125e3, 2125e3))
+ax.set_ylim((-0e3, 3000e3))
+#-2400000 -200000 2400000 3400000 \
 
 # Draw ETOPO1 background and coastline
-nc = Dataset('data/etopo1-northamerica.nc')
+nc = Dataset('data/etopo1-cordillera.nc')
 x = nc.variables['x']
 y = nc.variables['y']
 z = nc.variables['Band1']
@@ -46,12 +51,12 @@ cs = ax.contour(x[:], y[:], z[:], levels=[0],
 nc.close()
 
 # add lakes and rivers
-ax.add_feature(cfeature.NaturalEarthFeature(
-    category='physical', name='rivers_lake_centerlines', scale=scale,
-    edgecolor='#0978ab', facecolor='none', lw=1.0*bwu))
-ax.add_feature(cfeature.NaturalEarthFeature(
-    category='physical', name='lakes', scale=scale,
-    edgecolor='#0978ab', facecolor='#c6ecff', lw=0.5*bwu))
+#ax.add_feature(cfeature.NaturalEarthFeature(
+#    category='physical', name='rivers_lake_centerlines', scale=scale,
+#    edgecolor='#0978ab', facecolor='none', lw=1.0*bwu))
+#ax.add_feature(cfeature.NaturalEarthFeature(
+#    category='physical', name='lakes', scale=scale,
+#    edgecolor='#0978ab', facecolor='#c6ecff', lw=0.5*bwu))
 
 # add glaciers
 #ax.add_feature(cfeature.NaturalEarthFeature(
@@ -88,17 +93,13 @@ for age in raw_ages:
                 union = rec.geometry
             else:
                 union = union.union(rec.geometry)
-ax.add_geometries(union, proj, edgecolor='none', facecolor='#f5f4f2', alpha=0.75)
-ax.add_geometries(union, proj, edgecolor='#0978ab', facecolor='none', lw=0.5*bwu)
+ax.add_geometries(union, cal, edgecolor='none', facecolor='#f5f4f2', alpha=0.75)
+ax.add_geometries(union, cal, edgecolor='#0978ab', facecolor='none', lw=0.5*bwu)
 
 # add graticules
 ax.add_feature(cfeature.NaturalEarthFeature(
-    category='physical', name='graticules_15', scale=scale,
-    edgecolor='#000000', facecolor='none', lw=0.23*bwu))
-
-# add modelling domain
-ax.add_patch(plt.Rectangle((-2500e3, 0), 1500e3, 3000e3,
-                           ec='k', fc='none', lw=2.0*bwu, zorder=10))
+    category='physical', name='graticules_5', scale=scale,
+    edgecolor='#000000', facecolor='none', lw=0.25*bwu))
 
 # save
-fig.savefig('map-northamerica')
+fig.savefig('map-cordillera')
