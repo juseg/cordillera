5ee48a73fd31398c1cdfe6dc197e53358cc7b351
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-07-10 23:21:51 +0200

    Add glacial cycle snapshots.

---
+++ figures/plot-snapshots.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import sys
+
+sys.path.append('iceplot')
+
+import numpy as np
+from netCDF4 import Dataset
+from matplotlib import pyplot as plt
+from matplotlib.colors import BoundaryNorm, LogNorm, Normalize
+from mpl_toolkits.axes_grid1.axes_grid import ImageGrid
+from iceplot import plot as iplt
+
+s2ka = 1/(365.0 * 24 * 60 * 60 * 1000)
+
+# analysis functions
+def bounded_argmin(a, coord, bmin, bmax):
+    return np.ma.argmin(np.ma.array(a, mask=(coord < bmin)+(bmax < coord)))
+
+
+def bounded_argmax(a, coord, bmin, bmax):
+    return np.ma.argmax(np.ma.array(a, mask=(coord < bmin)+(bmax < coord)))
+
+# file path
+run_path = '/home/julien/pism/output/cordillera-narr-6km-bl/' \
+           'grip3222cool580+ccyc+till1545/y0120000'
+
+# initialize figure
+figw, figh = 170.0, 70.0
+fig = plt.figure(0, (figw/25.4, figh/25.4))
+ax = fig.add_axes([2.5/figw, 5.0/figh, 155.0/figw, 30.0/figh])
+
+# read output time series
+print 'reading time series...'
+nc = Dataset(run_path + '-ts.nc')
+time = nc.variables['time'][:]*s2ka
+ivol = nc.variables['slvol'][:]
+nc.close()
+
+# locate snapshot times
+tkalist = np.array([
+    bounded_argmin(ivol, time, -45, -20),
+    bounded_argmin(ivol, time, -35, -30),
+    bounded_argmin(ivol, time, -30, -25),
+    bounded_argmin(ivol, time, -26, -20),
+    bounded_argmax(ivol, time, -20, -18),  # LGM
+    bounded_argmax(ivol, time, -16, -14),
+    10549,  # 10 ka
+    bounded_argmax(ivol, time, -12, -10),  # YD
+    10999,  # 10 ka
+])
+tkalist = time[tkalist]
+
+# plot time series
+print 'plotting time series...'
+ax.plot(time, ivol, color='#0978ab')
+ax.set_xlabel('model time (kyr)')
+ax.set_ylabel('ice volume (m s.-l. eq.)')
+ax.yaxis.set_ticks_position('right')
+ax.yaxis.set_label_position('right')
+ax.grid(axis='y', c='0.5', ls='-', lw=0.1)
+for tka in tkalist:
+    ax.axvline(tka, color='0.5',  lw=0.1)
+
+# add image grid
+rect = [2.5/figw, 37.5/figh, 160.0/figw, 30.0/figh]
+grid = ImageGrid(fig, rect, (1, len(tkalist)), axes_pad=2.5/25.4,
+                 cbar_pad=2.5/25.4, cbar_size=2.5/25.4,
+                 cbar_mode='single', cbar_location='right')
+for ax in grid:
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+
+
+# read extra output
+print 'reading extra output...'
+nc = Dataset(run_path + '-extra.nc')
+time = nc.variables['time'][:]*s2ka
+
+# plot snapshots
+for i, tka in enumerate(tkalist):
+    print 'plotting %s ka snapshot...' % tka
+    ax = grid[i]
+
+    # find nearest slice
+    t = (np.abs(time[:]-tka)).argmin()
+
+    # bed topography
+    im = iplt.imshow(nc, 'topg', t, ax)
+    iplt.shading(nc, 'topg', t, ax)
+
+    # surface velocity
+    im = iplt.imshow(nc, 'velsurf_mag', t, ax,
+                     cmap='Blues', norm=LogNorm(1e1, 1e4), alpha=0.75)
+
+    # surface topography
+    cs = iplt.contour(nc, 'usurf', t, ax, levels=range(1000, 6000, 1000),
+                      cmap=None, colors='k', linewidths=0.10)
+
+    # ice margin
+    cs = iplt.icemargin(nc, t, ax)
+
+    # annotate time
+    ax.text(0.9, 0.95, '%s ka' % tka, ha='right', va='top',
+            bbox=dict(ec='k', fc='w', alpha=1.0),
+            transform=ax.transAxes)
+
+# close extra file
+nc.close()
+
+# add colorbar
+cb = fig.colorbar(im, ax.cax)
+
+# save
+print 'saving...'
+fig.savefig('plot-snapshots')
