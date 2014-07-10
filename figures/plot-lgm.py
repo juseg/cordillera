949c4145c6745836c2fd4c543c34fc70f95e289b
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-07-10 15:06:54 +0200

    Enhance LGM velocity map.

--- figures/plot-lgm.py
+++ figures/plot-lgm.py
@@ -8,6 +8,7 @@ sys.path.append('iceplot')
 import numpy as np
 from netCDF4 import Dataset
 from matplotlib import pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 from iceplot import plot as iplt
 
 # file path
@@ -17,38 +18,40 @@ filename = '/home/julien/pism/archive/cordillera-climate-3km/' \
 t = -1
 
 # prepare figure
-fig = plt.figure(0, (80/25.4, 160/25.4))
+figw, figh = 112.5, 225.0
+fig = plt.figure(0, (figw/25.4, figh/25.4))
 ax  = fig.add_axes([0, 0, 1, 1])
 ax.axis('off')
 nc = Dataset(filename)
 
 # bed topography
-im = iplt.imshow(nc, 'topg', t)
+im = iplt.imshow(nc, 'topg', t, zorder=0)
+iplt.shading(nc, 'topg', t, zorder=0)
 
-# velocity quiver (use old varnames {u,v}_ssa)
-# this does not work because the mask variable is missing.
-#qv = iplt.quiver(nc, 'velsurf', t, width=0.0005) #, scale = 250
-#qv.set_rasterized(True)
+# frozen bed areas
+cs = iplt.contourf(nc, 'temppabase', t, levels=[-50.0, -1e-6], zorder=1,
+                   cmap=None, colors='none', hatches=['//'])
+cs = iplt.contour(nc, 'temppabase', t, levels=[-50.0, -1e-6], zorder=1,
+                  cmap=None, colors='k', linewidths=0.25, linestyles=['-'])
 
-# surface topography contours
-#cs = iplt.contour(nc, 'usurf', t, levels=range(100, 6000, 100),
-#                  cmap=None, colors='k', linewidths=0.1)
-#cs = iplt.contour(nc, 'usurf', t, levels=range(1000,6000,1000),
-#                  cmap=None, colors='k', linewidths=0.5)
-#cs.clabel(fontsize=6, fmt='%g')
+# surface velocity
+im = iplt.imshow(nc, 'csurf', t, cmap='Blues', alpha=0.75, zorder=2)
+
+# surface topography
+cs = iplt.contour(nc, 'usurf', t, levels=range(100, 6000, 100),
+                  cmap=None, colors='k', linewidths=0.10, zorder=3)
+cs = iplt.contour(nc, 'usurf', t, levels=range(1000,6000,1000),
+                  cmap=None, colors='k', linewidths=0.25, zorder=3)
+cs.clabel(fontsize=6, fmt='%g', linewidths=0.5)
 
 # ice margin
-#cs = iplt.icemargin(nc, t)
+cs = iplt.icemargin(nc, t)
 nc.close()
 
 # colorbars
-#cax = fig.add_axes([20/800., 300/1600., 10/800., 200/1600.])
-#cb = mplt.colorbar(im, cax=cax)
-#cb.set_label('basal topography (m)')
-#cax = fig.add_axes([60/800., 300/1600., 10/800., 200/1600.])
-#cb = mplt.colorbar(qv, cax=cax)
-#cb.set_label('ice surface velocity (m/a)')
+cax = fig.add_axes([5/figw, 10/figh, 2.5/figw, 40/figh])
+cb = plt.colorbar(im, cax=cax)
+cb.set_label('ice surface velocity (m/a)')
 
 # save
 fig.savefig('plot-lgm')
-
