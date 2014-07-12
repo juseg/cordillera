ecdaecbae3a2feee248f0dbb70e93058685f0ddc
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-07-12 18:35:08 +0200

    Enhance snapshots plot.

--- figures/plot-snapshots.py
+++ figures/plot-snapshots.py
@@ -9,6 +9,9 @@ import numpy as np
 from netCDF4 import Dataset
 from matplotlib import pyplot as plt
 from matplotlib.colors import BoundaryNorm, LogNorm, Normalize
+from matplotlib.gridspec import GridSpec
+from matplotlib.transforms import blended_transform_factory
+from matplotlib.patches import Polygon
 from mpl_toolkits.axes_grid1.axes_grid import ImageGrid
 from iceplot import plot as iplt
 
@@ -26,11 +29,6 @@ def bounded_argmax(a, coord, bmin, bmax):
 run_path = '/home/julien/pism/output/cordillera-narr-6km-bl/' \
            'grip3222cool580+ccyc+till1545/y0120000'
 
-# initialize figure
-figw, figh = 170.0, 70.0
-fig = plt.figure(0, (figw/25.4, figh/25.4))
-ax = fig.add_axes([2.5/figw, 5.0/figh, 155.0/figw, 30.0/figh])
-
 # read output time series
 print 'reading time series...'
 nc = Dataset(run_path + '-ts.nc')
@@ -39,18 +37,35 @@ ivol = nc.variables['slvol'][:]
 nc.close()
 
 # locate snapshot times
-tkalist = np.array([
+idxlist = [
     bounded_argmin(ivol, time, -45, -20),
     bounded_argmin(ivol, time, -35, -30),
     bounded_argmin(ivol, time, -30, -25),
     bounded_argmin(ivol, time, -26, -20),
     bounded_argmax(ivol, time, -20, -18),  # LGM
     bounded_argmax(ivol, time, -16, -14),
-    10549,  # 10 ka
+    10699,  # 13 ka
     bounded_argmax(ivol, time, -12, -10),  # YD
     10999,  # 10 ka
-])
-tkalist = time[tkalist]
+]
+tkalist = time[idxlist]
+ivolist = ivol[idxlist]
+ntka = len(tkalist)
+
+# initialize figure
+figw, figh = 170.0, 80.0
+fig = plt.figure(0, (figw/25.4, figh/25.4))
+gs = GridSpec(2, ntka)
+ax = plt.subplot(gs[0, :])
+ax = tsax = plt.subplot(gs[1, :])
+fig.subplots_adjust(left=2.5/figw, right=1-12.5/figw,
+                    bottom=5.0/figh, top=1-2.5/figh,
+                    wspace=1/((1+figw/2.5)/ntka-1),
+                    hspace=1/((1+figh/12.5)/2-1))
+
+# coordinate transformations
+blendtrans = blended_transform_factory(tsax.transData, tsax.transAxes)
+figinvtrans = fig.transFigure.inverted()
 
 # plot time series
 print 'plotting time series...'
@@ -62,16 +77,7 @@ ax.yaxis.set_label_position('right')
 ax.grid(axis='y', c='0.5', ls='-', lw=0.1)
 for tka in tkalist:
     ax.axvline(tka, color='0.5',  lw=0.1)
-
-# add image grid
-rect = [2.5/figw, 37.5/figh, 160.0/figw, 30.0/figh]
-grid = ImageGrid(fig, rect, (1, len(tkalist)), axes_pad=2.5/25.4,
-                 cbar_pad=2.5/25.4, cbar_size=2.5/25.4,
-                 cbar_mode='single', cbar_location='right')
-for ax in grid:
-    ax.xaxis.set_visible(False)
-    ax.yaxis.set_visible(False)
-
+#ax.plot(tkalist, ivolist, color='#0978ab', marker='+', ls='')  # add crosses
 
 # read extra output
 print 'reading extra output...'
@@ -81,7 +87,9 @@ time = nc.variables['time'][:]*s2ka
 # plot snapshots
 for i, tka in enumerate(tkalist):
     print 'plotting %s ka snapshot...' % tka
-    ax = grid[i]
+    ax = plt.subplot(gs[0, i])
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
 
     # find nearest slice
     t = (np.abs(time[:]-tka)).argmin()
@@ -106,11 +114,20 @@ for i, tka in enumerate(tkalist):
             bbox=dict(ec='k', fc='w', alpha=1.0),
             transform=ax.transAxes)
 
+    # add triangle
+    xy1 = figinvtrans.transform(blendtrans.transform([tkalist[i], 1.0]))
+    xy2 = figinvtrans.transform(ax.transAxes.transform([0.0, 0.0]))
+    xy3 = figinvtrans.transform(ax.transAxes.transform([1.0, 0.0]))
+    tri = Polygon([xy1, xy2, xy3], closed=True, color='0.9', lw=0.1,
+                  zorder=-9, transform=fig.transFigure)
+    fig.patches.append(tri)
+
 # close extra file
 nc.close()
 
-# add colorbar
-cb = fig.colorbar(im, ax.cax)
+# add subfigure labels
+fig.text(5/figw, 43.75/figh, '(a)', fontweight='bold')
+fig.text(5/figw, 32.5/figh, '(b)', fontweight='bold')
 
 # save
 print 'saving...'
