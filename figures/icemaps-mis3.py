bd12091f61b6b9b41f6d55b2f57462655d51c006
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-09-09 18:07:19 +0200

    Add MIS3 icemaps.

---
+++ figures/icemaps-mis3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+from paperglobals import darkred
+from icemaps import icemaps
+
+# plot
+fig = icemaps(mis=3)
+
+# save
+print('saving...')
+fig.savefig('icemaps-mis3')
