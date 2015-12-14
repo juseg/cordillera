d501071beb41f571f401bef6ea101be7733ded91
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2015-12-14 11:22:39 +0100

    Add plot for delta and Wmax effect on Ntil.

---
+++ figures/sens-ntil.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+from paperglobals import *
+
+
+def plot_Ntil(W,
+              ax=None,
+              delta=0.02,  # -      (Bueler and van Pelt, 2015)
+              Wmax=2.0,    # m      (Bueler and van Pelt, 2015)
+              h=1000.0,    # m      (Bueler and van Pelt, 2015)
+              e0=0.69,     # -      (Bueler and van Pelt, 2015)
+              Cc=0.12,     # -      (Bueler and van Pelt, 2015)
+              rho=910.0,   # kg m-3 (Bueler and van Pelt, 2015)
+              g=9.81,      # m s-2  (Bueler and van Pelt, 2015)
+              **kwargs):
+    """Plot effective pressure on till as a function of water level in the
+    style of Bueler and van Pelt (2015, Fig. 1).
+    Default parameters are from Bueler and van Pelt (2015, Table 1)."""
+    
+    # get current axes if None is given
+    ax = ax or iplt.gca()
+
+    # compute bound and unbound Ntil
+    P0 = rho * g * h
+    Ntil_cap = delta * P0 * 10**(e0/Cc*(1-(W/Wmax)))
+    Ntil = np.minimum(P0, Ntil_cap)
+    Ntil_cap = np.ma.array(Ntil, mask=(W<Wmax))
+    Ntil = np.ma.array(Ntil, mask=(W>Wmax))
+    
+    # plot in bars
+    ax.plot(W, Ntil_cap*1e-5, ls='--', **kwargs)
+    ax.plot(W, Ntil*1e-5, ls='-', **kwargs)
+    ax.plot(Wmax, delta*P0*1e-5, marker='o', **kwargs)
+
+
+if __name__ == '__main__':
+
+    # pressure-adjusted temperature
+    W = np.linspace(0.0, 4.0, 101)
+
+    # initialize figure
+    fig, grid = iplt.subplots_mm(2, 1, sharex=True, figsize=(85.0, 100.0),
+                                 left=10.0, right=2.5, bottom=7.5, top=2.5,
+                                 wspace=2.5, hspace=2.5)
+
+    # for each axes
+    for ax in grid:
+
+        # plot default run
+        plot_Ntil(W, ax=ax, c='k')
+
+        # plot with different Wmax
+        for Wmax in [1.0, 3.0]:
+            plot_Ntil(W, ax=ax, Wmax=Wmax, c=colors[0])
+
+        # plot with different delta
+        for delta in [0.01, 0.05]:
+            plot_Ntil(W, ax=ax, delta=delta, c=colors[2])
+
+        # set axes properties
+        ax.set_ylabel('Ntil (bar)')
+        ax.grid()
+
+    # linear scale on top axes
+    grid[0].set_ylim(-2.0, 100.0)
+
+    # log scale on bottom axes
+    grid[1].set_xlabel('W (m)')
+    grid[1].set_ylim(2e-1, 2e2)
+    grid[1].set_yscale('log')
+
+    # save
+    fig.savefig('sens_ntil')
