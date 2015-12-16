01d26d860768b927b8f4bbbb53007058f49c9aa9
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2015-12-16 17:28:48 +0100

    Fix printmaxarea.py.

--- figures/printmaxarea.py
+++ figures/printmaxarea.py
@@ -6,10 +6,11 @@ import numpy as np
 import util as ut
 import iceplotlib.plot as iplt
 
+res = '10km'
 offsets = np.arange(5.7, 7.0, 0.1)
 
 # print table header
-print '\n    dt ' + '| %7s ' * 6 % tuple(records),
+print '\n    dt ' + '| %7s ' * 6 % tuple(ut.records),
 print '\n  -----' + '+---------' * 6,
 
 # for required temperature offset values
@@ -17,7 +18,7 @@ for i, dt in enumerate(offsets):
     print '\n   ' + '%3.1f' % dt,
 
     # for each record
-    for j, rec in enumerate(records):
+    for j, rec in enumerate(ut.records):
 
         # try to print MIS2 area
         try:
@@ -30,16 +31,16 @@ for i, dt in enumerate(offsets):
             thk = nc.variables['thk']
             mask = nc.variables['mask']
             time = nc.variables['time']
-            idx = np.abs(time[:]-t*a2s).argmin()
+            idx = np.abs(time[:]-t*ut.a2s).argmin()
 
             # to compute maximum grounded ice area
-            #iarea = ((thk[:] >= thkth)*(mask[:] == 2)).sum(axis=(1, 2)).max()*1e-4
+            #iarea = ((thk[:] >= ut.thkth)*(mask[:] == 2)).sum(axis=(1, 2)).max()*1e-4
 
             # to compute grounded ice area at MIS 2
-            iarea = ((thk[idx] >= thkth)*(mask[idx] == 2)).sum()*1e-4
+            iarea = ((thk[idx] >= ut.thkth)*(mask[idx] == 2)).sum()*1e-4
 
             # to compute area ever covered by grounded ice or footprint
-            #iarea = ((thk[:] >= thkth)*(mask[:] == 2)).any(axis=(0)).sum()*1e-4
+            #iarea = ((thk[:] >= ut.thkth)*(mask[:] == 2)).any(axis=(0)).sum()*1e-4
 
             # print
             nc.close()
