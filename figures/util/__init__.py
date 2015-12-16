004ce98ed9017da58f99d509d1f9d39ebcbc4eec
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2015-12-16 17:29:10 +0100

    Update offsets to grounded ice area target.

--- figures/util/__init__.py
+++ figures/util/__init__.py
@@ -32,7 +32,7 @@ records = ['grip', 'ngrip', 'epica', 'vostok', 'odp1012', 'odp1020']
 labels = ['GRIP', 'NGRIP', 'EPICA', 'Vostok', 'ODP 1012', 'ODP 1020']
 colors = ['#1F78B4', '#A6CEE3', '#E31A1C', '#FB9A99', '#33A02C', '#B2DF8A']
 markers = ['s', 'D', 'o', 'h', 'v', '^']
-offsets = [6.1, 6.5, 5.9, 5.9, 6.1, 6.0]
+offsets = [6.2, 6.6, 5.9, 6.0, 6.1, 6.0]
 
 # default thickness threshold
 thkth = 1.0
