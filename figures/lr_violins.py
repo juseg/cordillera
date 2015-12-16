1b403f1f6c406f13b07cdf25b485549ebb95b6c0
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2015-12-16 14:31:32 +0100

    Make violin plot prettier.

--- figures/lr_violins.py
+++ figures/lr_violins.py
@@ -20,10 +20,24 @@ for i, rec in enumerate(ut.records):
     data.append(slvol)
 
 # plot
-iplt.violinplot(data)
+violins = iplt.violinplot(data)
 
-# set axes properties and save
-print 'saving violins...'
+# set colors
+print violins
+for p, c in zip(violins['bodies'], ut.colors):
+    p.set_color(c)
+violins['cbars'].set_color('0.5')
+violins['cmins'].set_color('0.5')
+violins['cmaxes'].set_color('0.5')
+
+# set axes properties
 ax.set_ylim(0.0, 10.0)
 ax.set_ylabel('ice volume (m s.-l. eq.)')
+ax.set_xlim(0.5, 6.5)
+ax.set_xticks(np.arange(len(ut.records))+1)
+ax.set_xticklabels(ut.labels)
+ax.set_xlabel('record')
+
+# save
+print 'saving violins...'
 fig.savefig('lr_violins')
