a130da772b68aad886b7b4944b8896092f3ee332
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2015-12-20 12:12:10 +0100

    Fill between sensitivity time series.

--- figures/sens_ts.py
+++ figures/sens_ts.py
@@ -5,6 +5,20 @@ import util as ut
 import numpy as np
 import iceplotlib.plot as iplt
 
+
+def fill_between_safe(x, y1, y2, ax=None, **kwargs):
+    """Fill between even if y1 and y2 have different lenght."""
+
+    # truncate two arrays to smaller lenght
+    minlen = min(len(y1), len(y2))
+    y1 = y1[:minlen]
+    y2 = y2[:minlen]
+
+    # fill between
+    ax = ax or iplt.gca()
+    return ax.fill_between(x, y1, y2, **kwargs)
+
+
 # parameters
 res = '10km'
 rec = 'grip'
@@ -16,6 +30,10 @@ fig, grid = iplt.subplots_mm(nrows=2, ncols=1, sharex=True, sharey=True,
                              figsize=(figw, figh),
                              left=10.0, right=2.5, bottom=10.0, top=2.5,
                              wspace=2.5, hspace=2.5)
+
+# initialize lists
+time_series = []
+ivol_series = []
 
 # loop on records
 tabline = ' '*4 + '%-30s '+ '& %6.2f '*3
@@ -46,26 +64,32 @@ for i, conf in enumerate(ut.sens_configs):
     print tabline % ( ('',) + tuple(mis_iareas) )
     print tabline % ( ('',) + tuple(mis_ivols) ) + '\\\\'
 
-    # plot reference run
-    if i == 0:
-        for ax in grid:
-            ax.plot(-ts_time, ts_ivol, color=ut.sens_colors[i], zorder=3)
+    # append to lists
+    time_series.append(ts_time)
+    ivol_series.append(ts_ivol)
 
+# plot reference run on top
+for ax in grid:
+    ax.plot(-time_series[0], ivol_series[0], color=ut.sens_colors[0],
+            alpha=0.75, zorder=3)
 
-    # plot sensitivity to rheologic parameters
-    elif i <=2:
-        grid[0].plot(-ts_time, ts_ivol, color=ut.sens_colors[i])
+# plot sensitivity to rheologic parameters
+fill_between_safe(-time_series[1], ivol_series[1], ivol_series[2], ax=grid[0],
+                  edgecolor=ut.sens_colors[1], facecolor=ut.sens_colors[2],
+                  alpha=0.75)
 
-    else:
-        grid[1].plot(-ts_time, ts_ivol, color=ut.sens_colors[i])
+# plot sensitivity to sliding parameters
+fill_between_safe(-time_series[3], ivol_series[3], ivol_series[4], ax=grid[1],
+                  edgecolor=ut.sens_colors[3], facecolor=ut.sens_colors[4],
+                  alpha=0.75)
 
 # set axes properties
 for ax in grid:
 
     # mark true MIS stages
     # source: http://www.lorraine-lisiecki.com/LR04_MISboundaries.txt
-    ax.axvspan(71, 57, fc='0.85', lw=0.25)
-    ax.axvspan(29, 14, fc='0.85', lw=0.25)
+    ax.axvspan(71, 57, fc='0.85', lw=0.25, zorder=0)
+    ax.axvspan(29, 14, fc='0.85', lw=0.25, zorder=0)
     ax.text((120+71)/2, 9.0, 'MIS 5', ha='center')
     ax.text((71+57)/2, 0.5, 'MIS 4', ha='center')
     ax.text((57+29)/2, 9.0, 'MIS 3', ha='center')
