3715db59798967e5fa1b592c756e8c5e0f159d6b
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2015-12-30 11:59:42 +0100

    Change order of sensitivity labels in figs.
    New order is 'Default', 'Soft ice', 'Hard ice', 'Soft bed', 'Hard bed'.

--- figures/util/sens.py
+++ figures/util/sens.py
@@ -4,10 +4,10 @@
 """Parameters for sensitivity tests."""
 
 configs = ['ccyc4+till1545',
-           'ccyc4+rheocp10+till1545',
            'ccyc4+rheocp10+esia5+till1545',
-           'ccyc4+d050+wmax05m+till1545',
-           'ccyc4+d010+wmax01m+till1545']
-labels = ['Default', 'Hard ice', 'Soft ice', 'Hard bed', 'Soft bed']
-offsets = [6.2, 6.0, 6.6, 5.9, 6.5]
-colors = ['k', '#ff7f00', '#fdbf6f', '#6a3d9a', '#cab2d6']
+           'ccyc4+rheocp10+till1545',
+           'ccyc4+d010+wmax01m+till1545',
+           'ccyc4+d050+wmax05m+till1545']
+labels = ['Default', 'Soft ice', 'Hard ice', 'Soft bed', 'Hard bed']
+offsets = [6.2, 6.6, 6.0, 6.5, 5.9]
+colors = ['k', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a']
