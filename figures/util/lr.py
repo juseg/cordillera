9fddd21d9f278e99256f19a0251c52fed0f70b15
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-01-19 09:16:46 +0100

    Change ODP 1020 offset from 6.0 to 5.9.
    This yield MIS 2 area slightly closer to the 2.1 target, although I can't explain why area increase with a lower offset.

--- figures/util/lr.py
+++ figures/util/lr.py
@@ -7,4 +7,7 @@ records = ['grip', 'ngrip', 'epica', 'vostok', 'odp1012', 'odp1020']
 labels = ['GRIP', 'NGRIP', 'EPICA', 'Vostok', 'ODP 1012', 'ODP 1020']
 colors = ['#1F78B4', '#A6CEE3', '#E31A1C', '#FB9A99', '#33A02C', '#B2DF8A']
 markers = ['s', 'D', 'o', 'h', 'v', '^']
-offsets = [6.2, 6.6, 5.9, 6.0, 6.1, 6.0]
+offsets = [6.2, 6.6, 5.9, 6.0, 6.1, 5.9]
+
+# 32 to 22 ka offsets before scaling
+noscale = [-16.4126, -26.7098, -9.2055, -7.9550, -3.7889, -5.0000]
