9f4cb73fdf21de608c1ee2a62206f258fac54f92
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-11-09 00:32:53 +0100

    Post-merge: replace data scripts by Makefile.
    Just spent several hours just on that. I hate Makefile syntax.

--- data/get-dykeshp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-url='http://ftp.geogratis.gc.ca/pub/nrcan_rncan/publications/ess_sst/214/214399/of_1574.zip'
-archive=$(basename $url)
-
-[ -f $archive ] || wget $url
-unzip -j $archive data/shp/ice??k.{dbf,shp,shx}
-touch ice??k.{dbf,shp,shx}
