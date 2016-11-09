c3a39e2bcb9cd00e9530de7a45db87791f09c1b6
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-11-09 11:54:36 +0100

    Post-merge: move cisghf processing in data dir.

--- data/postprocess-output.sh
+++ data/postprocess-output.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 # output dir and variables to include
-odir="/home/juliens/pism/output/0.7.2/cordillera-narr-5km"
+odir="$HOME/pism/output/0.7.2/cordillera-narr-5km"
 vars="lon,lat,bwat,bmelt,thk"
 
 # config and geoflux arguments
 confargs=( "cgeo1+hynull" "cgeo1" "cgeo1" "cgeo1" "cgeo1" "cgeo1" )
 gflxargs=( "" "" "+dav13" "+gou11comb" "+gou11simi" "+sha04" )
 
+# make directory or update modification date
+mkdir -p processed
+touch processed
+cd processed
+
 # for each of the six runs
 for i in {0..5}
 do
 
-    # get file name
+    # get run name
     conf=${confargs[i]}
     gflx=${gflxargs[i]}
     runname="grip3222cool620+${conf}+till1545${gflx}"
-    ifiles="$odir/$runname/y???????-extra.nc"
 
-    # compute sum in subprocess
+    # compute sum
+    echo "processing $runname ..."
+    ifile="$odir/$runname/y???????-extra.nc"
     ofile="$runname.avg.nc"
-    echo "preparing $ofile..."
-    ncra -O -d time,,,1 -v $vars $ifiles $ofile
+    ncra -O -d time,,,1 -v $vars $ifile $ofile
 
 done
