12c41b7b0e2f79ffe4cd403c3ba213b2e2f75207
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-09-26 19:50:25 +0200

    Update cycle animation to 5 km resolution.

---
+++ figures/anim_cordillera_cycle.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+name="anim_cordillera_cycle"
+frames="/scratch_net/iceberg/juliens/anim/$name/%04d00.png"
+options="-i $frames -r 25 -pix_fmt yuv420p"
+
+avconv $options -c:v libx264 $name.mp4 -y
+avconv $options -c:v theora $name.ogg -y
