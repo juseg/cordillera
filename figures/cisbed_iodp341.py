446b5c4f924dafb904ad7b8c19d0272384e40f22
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2020-01-12 20:16:34 +0100

    Use postprocessed data in IODP341 figure.

--- figures/cisbed_iodp341.py
+++ figures/cisbed_iodp341.py
@@ -5,9 +5,9 @@
 
 """Plot Cordillera bedrock IODP 341 time series."""
 
-import os
+import glob
+import xarray as xr
 import absplots as apl
-import pismx.open
 import util
 
 
@@ -18,42 +18,21 @@ def main():
     fig, tsax = apl.subplots_mm(figsize=(170, 60), gridspec_kw=dict(
         left=12.5, right=2.5, bottom=7.5, top=5, hspace=2.5, wspace=2.5))
 
-    # for each record
-    for i, rec in enumerate(util.cisbed_records):
-        color = util.cisbed_colours[i]
-        offset = util.cisbed_offsets[i]
+    # for each run
+    for run in sorted(glob.glob('../data/processed/*.iodp341.nc')):
+        color = 'C{:d}'.format(4*('epica' in run) + 1*('cisbed' in run))
+        label = r'{}, {}, $\nu_m = 10^{{{}}}\,Pa\,s$'.format(
+            'EPICA' if 'epica' in run else 'GRIP',
+            'new' if 'cisbed' in run else 'old',
+            '21' if 'num1e21' in run else '19')
+        style = ':' if 'num1e21' in run else '-'
 
-        # for each configuration
-        rundirs = ['0.7.2-craypetsc/ciscyc4.10km.{}.{:04.0f}',
-                   '1.1.3/cisbed4.10km.{}.{:04.0f}.gou11simi',
-                   '1.1.3/cisbed4.10km.{}.{:04.0f}.gou11simi.num1e21']
-        for rundir in rundirs:
-            color = 'C{:d}'.format(4*(rec == 'EPICA') + 1*('cisbed' in rundir))
-            label = r'{}, {}, $\nu_m = 10^{{{}}}\,Pa\,s$'.format(
-                rec, 'new' if 'cisbed' in rundir else 'old',
-                '21' if 'num1e21' in rundir else '19')
-            style = ':' if 'num1e21' in rundir else '-'
-
-            # load ice volume time series
-            # run 1.1.3/cisbed4.5km.epica.590.gou11simi.num1e21 has issues
-            with pismx.open.mfdataset(
-                    os.path.expanduser('~/pism/output/{}/ex.???????.nc'.format(
-                        rundir.format(rec.lower(), 100*offset)))) as data:
-
-                # select Hole U1420A, 59°41.3399′N, 143°12.0599′W
-                # data[:, j, i] "is not supported because the ordering of
-                # dimensions [...] can vary between different arrays"
-                # so we use dist.dims (differs between PISM 0.7 and 1.0).
-                lon, lat = -143.200998, 59.688998
-                dist = (data.lat-lat)**2 + (data.lon-lon)**2
-                i, j = divmod(int(dist.argmin()), dist.shape[-1])
-                data = data.isel({dist.dims[-2]: i, dist.dims[-1]: j})
-
-                # plot ice volume time series
-                data.topg.plot(ax=tsax, color=color, label=label, ls=style)
+        # plot ice volume time series
+        with xr.open_dataset(run) as data:
+            data.topg.plot(ax=tsax, color=color, label=label, ls=style)
 
     # set time series axes properties
-    tsax.set_title('Model output at IODP U1420A, 59°41′N, 143°12′W')
+    tsax.set_title('Model output at IODP 341, hole U1420A, 59°41′N, 143°12′W')
     tsax.set_xlim(40.0, 0.0)
     tsax.legend()
 
