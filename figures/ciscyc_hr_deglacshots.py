c63bd4457e51813ab40625940819346ccb193ecc
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2020-01-16 14:34:45 +0100

    Update deglaciation snaps (absplots, xarray, etc).

--- figures/ciscyc_hr_deglacshots.py
+++ figures/ciscyc_hr_deglacshots.py
@@ -1,63 +1,75 @@
-#!/usr/bin/env python2
-# coding: utf-8
+#!/usr/bin/env python
+# Copyright (c) 2014--2020, Julien Seguinot <seguinot@vaw.baug.ethz.ch>
+# Creative Commons Attribution-ShareAlike 4.0 International License
+# (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
+
+"""
+Plot CISCYC hires deglaciation snapshots.
+"""
 
 import util as ut
-import numpy as np
-import iceplotlib.plot as iplt
+import xarray as xr
+import absplots as apl
 
-# parameters
-times = [-16e3, -14e3, -12e3, -10e3]
 
-# initialize figure
-figw, figh = 135.0, 115.0
-fig, grid = iplt.subplots_mm(nrows=2, ncols=4, sharex=True, sharey=True,
-                             figsize=(figw, figh), projection=ut.pl.proj,
-                             left=2.5, right=17.5, bottom=2.5, top=2.5,
-                             wspace=2.5, hspace=2.5)
-cax = fig.add_axes([1-15.0/figw, 2.5/figh, 5.0/figw, 1-5.0/figh])
+def main():
+    """Main program called during execution."""
 
-# loop on records
-for i, rec in enumerate(ut.ciscyc_hr_records):
-    dt = ut.ciscyc_hr_offsets[i]
+    # initialize figure
+    fig, grid = apl.subplots_mm(
+        figsize=(135, 115), nrows=2, ncols=4, gridspec_kw=dict(
+            left=2.5, right=17.5, bottom=2.5, top=2.5, wspace=2.5, hspace=2.5),
+        subplot_kw=dict(projection=ut.pl.proj))
+    cax = fig.add_axes_mm([120, 2.5, 5, 110])
 
-    # load extra output
-    nc = ut.io.load('output/0.7.2-craypetsc/cordillera-narr-5km/'
-                    '%s3222cool%03d+ccyc4+till1545/y???????-extra.nc'
-                     % (rec.replace(' ', '').lower(), round(100*dt)))
+    # loop on records
+    for i, rec in enumerate(ut.ciscyc_hr_records):
 
-    # plot
-    for j, t in enumerate(times):
-        ax = grid[i, j]
-        ax.set_rasterization_zorder(2.5)
-        nc.imshow('topg', ax=ax, t=t, cmap=ut.topo_cmap, norm=ut.topo_norm,
-                  zorder=-1)
-        nc.contour('topg', ax=ax, t=t, levels=[0.0], cmap=None,
-                   colors='0.25', linewidths=0.25, zorder=0)
-        nc.icemargin(ax=ax, t=t, linewidths=0.5)
-        nc.contour('usurf', ax=ax, t=t, levels=range(200, 5000, 200),
-                   cmap=None, colors='k', linewidths=0.1)
-        nc.contour('usurf', ax=ax, t=t, levels=range(1000, 5000, 1000),
-                   cmap=None, colors='k', linewidths=0.25)
-        im = nc.imshow('velsurf_mag', ax=ax, t=t,
-                       cmap=ut.vel_cmap, norm=ut.vel_norm, alpha=0.75)
-        ut.pl.draw_natural_earth(ax)
-        ut.pl.add_corner_tag(ax, '%s ka' % (-t/1e3))
+        # open postprocessed output
+        ds = xr.load_dataset(
+            '../data/processed/ciscyc.5km.{}.ex.1ka.nc'.format(rec.lower()))
 
-        # add profile lines
-        for k, yp in enumerate([1.7e6, 1.4e6, 1.1e6, 0.8e6]):
-            ax.plot([-2.4e6, -1.25e6], [yp, yp], 'k|',
-                    lw=0.25, ls='--', dashes=(2, 2))
-            if j==3:
-                ax.text(-1.2e6, yp, chr(65+k), ha='left', va='bottom')
+        # plot
+        innerlevs = [l for l in range(200, 5000, 200) if l % 1000 != 0]
+        outerlevs = [l for l in range(200, 5000, 200) if l % 1000 == 0]
+        for j, age in enumerate([16, 14, 12, 10]):
+            ax = grid[i, j]
+            ax.set_rasterization_zorder(2.5)
+            snap = ds.sel(age=age)
+            snap.topg.plot.imshow(
+                ax=ax, add_colorbar=False, cmap=ut.topo_cmap,
+                norm=ut.topo_norm, zorder=-1)
+            snap.topg.plot.contour(
+                ax=ax, levels=[0], colors='0.25', linewidths=0.25, zorder=0)
+            snap.thk.notnull().plot.contour(
+                ax=ax, colors='0.25', levels=[0], linewidths=0.5)
+            (snap.topg+snap.thk).plot.contour(
+                ax=ax, colors='0.25', levels=innerlevs, linewidths=0.1)
+            (snap.topg+snap.thk).plot.contour(
+                ax=ax, colors='0.25', levels=outerlevs, linewidths=0.25)
+            ((snap.uvelsurf**2+snap.vvelsurf**2)**0.5).plot.imshow(
+                ax=ax, alpha=0.75, cbar_ax=cax,
+                cmap=ut.vel_cmap, norm=ut.vel_norm, cbar_kwargs=dict(
+                    label=r'Surface velocity ($m\,a^{-1}$)',))
 
-    # add record label
-    ut.pl.add_corner_tag(ax, rec.upper(), va='bottom')
+            # add map elements
+            ax.set_title('')
+            ut.pl.draw_natural_earth(ax)
+            ut.pl.add_corner_tag(ax, '{} ka'.format(age))
 
-    # close extra file
-    nc.close()
+            # add profile lines
+            for k, yp in enumerate([1.7e6, 1.4e6, 1.1e6, 0.8e6]):
+                ax.plot([-2.4e6, -1.25e6], [yp, yp], 'k|',
+                        lw=0.25, ls='--', dashes=(2, 2))
+                if j == 3:
+                    ax.text(-1.2e6, yp, chr(65+k), ha='left', va='bottom')
 
-# add colorbar and save
-cb = fig.colorbar(im, cax, extend='both', format='%i',
-                  ticks=np.logspace(1, 3.5, 6))
-cb.set_label(r'surface velocity ($m\,a^{-1}$)', labelpad=-2.0)
-ut.pl.savefig(fig)
+        # add record label
+        ut.pl.add_corner_tag(ax, rec.upper(), va='bottom')
+
+    # add colorbar and save
+    ut.pl.savefig(fig)
+
+
+if __name__ == '__main__':
+    main()
