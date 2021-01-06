64a16b0ab2d307e90ae0878cf0774abc11749159
Julien Seguinot <juseg@posteo.eu>
2021-01-06 21:48:03 +0100

    Comment leftover broken code in cycle movie.

--- movies/anim_cordillera_cycle.py
+++ movies/anim_cordillera_cycle.py
@@ -24,71 +24,64 @@ def draw(t):
     fig, grid, cax, tsax = utils.subplots()
     twax = tsax.twiny()
 
-    # add signature #FIXME move to util
-    figw, figh = [dim*25.4 for dim in fig.get_size_inches()]
-    fig.text(1-2.5/figw, 2.5/figh, 'J. Seguinot et al. (in prep.)',
-             ha='right', va='bottom')
-
-    # load boot topo
-    filepath = 'input/boot/cordillera-etopo1bed+thk+gou11simi-5km.nc'
-    nc = ut.io.load(filepath)
-    zref = nc.variables['topg'][:].T
-    nc.close()
-
     # for each record
     for i, rec in enumerate(['GRIP', 'EPICA']):
         ax = grid[i]
-        c = ut.cisbed_colours[i]
-        dt = ut.cisbed_offsets[i]
-        dt_file = '%s3222cool%04d' % (rec.lower(), round(dt*100))
-        run_dir = 'output/e9d2d1f/cordillera-narr-5km/%s+cisbed2+till1545' % dt_file
 
-        # load extra data
-        nc = ut.io.load(run_dir + '/y???????-extra.nc')
+# FIXME use pismx and fix file paths
+#        c = ut.cisbed_colours[i]
+#        dt = ut.cisbed_offsets[i]
+#        dt_file = '%s3222cool%04d' % (rec.lower(), round(dt*100))
+#        run_dir = 'output/e9d2d1f/cordillera-narr-5km/%s+cisbed2+till1545' % dt_file
+#
+#        # load extra data
+#        nc = ut.io.load(run_dir + '/y???????-extra.nc')
+#
+#        # plot
+#        im = nc.imshow('topg', ax, t, vmin=0.0, vmax=3e3, cmap='Greys', zorder=-1)
+#        cs = nc.contour('topg', ax, t, levels=[0.0], colors='0.25',
+#                        linewidths=0.25, zorder=0)
+#        im = nc.imshow('velsurf_mag', ax, t, norm=ut.pl.velnorm, cmap='Blues',
+#                       alpha=0.75)
+#        cs = nc.contour('usurf', ax, t, levels=ut.pl.inlevs,
+#                        colors='0.25', linewidths=0.1)
+#        cs = nc.contour('usurf', ax, t, levels=ut.pl.utlevs,
+#                        colors='0.25', linewidths=0.25)
+#        cs = nc.icemargin(ax, t, colors='k', linewidths=0.25)
+#
+#        # close extra data
+#        nc.close()
 
-        # plot
-        im = nc.imshow('topg', ax, t, vmin=0.0, vmax=3e3, cmap='Greys', zorder=-1)
-        cs = nc.contour('topg', ax, t, levels=[0.0], colors='0.25',
-                        linewidths=0.25, zorder=0)
-        im = nc.imshow('velsurf_mag', ax, t, norm=ut.pl.velnorm, cmap='Blues',
-                       alpha=0.75)
-        cs = nc.contour('usurf', ax, t, levels=ut.pl.inlevs,
-                        colors='0.25', linewidths=0.1)
-        cs = nc.contour('usurf', ax, t, levels=ut.pl.utlevs,
-                        colors='0.25', linewidths=0.25)
-        cs = nc.icemargin(ax, t, colors='k', linewidths=0.25)
-
-        # close extra data
-        nc.close()
 
         # add map elements
         utils.draw_natural_earth(ax, scale='50m')
         cde.add_subfig_label(rec, ax=ax, loc='nw')
         cde.add_subfig_label('{:.1f} ka'.format(-t/1e3), ax=ax, loc='ne')
 
-        # load temperature forcing
-        nc = ut.io.load('input/dt/%s.nc' % dt_file)
-        age = -nc.variables['time'][:]/1e3
-        dt = nc.variables['delta_T'][:]
-        nc.close()
-
-        # plot temperature forcing
-        mask = age>=-t/1e3
-        tsax.plot(dt[mask], age[mask], color=c, alpha=0.25)
-
-        # load time series data
-        nc = ut.io.load(run_dir + '/y???????-ts.nc')
-        age = -nc.variables['time'][:]/(1e3*365*24*60*60)
-        vol = nc.variables['slvol'][:]
-        nc.close()
-
-        # plot ice volume time series
-        mask = age>=-t/1e3
-        twax.plot(vol[mask], age[mask], color=c)
-
-    # add colorbar
-    cb = fig.colorbar(im, cax, extend='both', orientation='horizontal')
-    cb.set_label(r'surface velocity ($m\,a^{-1}$)')
+# FIXME use pismx and fix file paths
+#        # load temperature forcing
+#        nc = ut.io.load('input/dt/%s.nc' % dt_file)
+#        age = -nc.variables['time'][:]/1e3
+#        dt = nc.variables['delta_T'][:]
+#        nc.close()
+#
+#        # plot temperature forcing
+#        mask = age>=-t/1e3
+#        tsax.plot(dt[mask], age[mask], color=c, alpha=0.25)
+#
+#        # load time series data
+#        nc = ut.io.load(run_dir + '/y???????-ts.nc')
+#        age = -nc.variables['time'][:]/(1e3*365*24*60*60)
+#        vol = nc.variables['slvol'][:]
+#        nc.close()
+#
+#        # plot ice volume time series
+#        mask = age>=-t/1e3
+#        twax.plot(vol[mask], age[mask], color=c)
+#
+#    # add colorbar
+#    cb = fig.colorbar(im, cax, extend='both', orientation='horizontal')
+#    cb.set_label(r'surface velocity ($m\,a^{-1}$)')
 
     # set time series axes properties
     tsax.set_ylim(120.0, 0.0)
