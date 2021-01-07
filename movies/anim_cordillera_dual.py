f6ad3dc3d559601e738d230ff9d2ee3e8fe63200
Julien Seguinot <juseg@posteo.eu>
2021-01-07 08:39:10 +0100

    Cleanup and simplify dual animation code.

--- movies/anim_cordillera_dual.py
+++ movies/anim_cordillera_dual.py
@@ -3,7 +3,7 @@
 # Creative Commons Attribution-ShareAlike 4.0 International License
 # (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
-"""Cordillera glacial cycle animation."""
+"""Cordillera two-panel animation framework."""
 
 import os
 import multiprocessing as mp
@@ -14,58 +14,13 @@ import cartowik.decorations as cde
 import cartowik.naturalearth as cne
 import absplots as apl
 import pismx.open
-import utils
+
 
 # Color palette
 # -------------
 
 # set color cycle to colorbrewer Paired palette
 plt.rc('axes', prop_cycle=plt.cycler(color=plt.get_cmap('Paired').colors))
-
-
-# Figure creation
-# ---------------
-
-def subplots():
-    """Init figure with two subplots, bottom colorbar and timeseries."""
-
-    # geographic projection
-    proj = ccrs.LambertConformal(central_longitude=-95, central_latitude=49,
-                                 standard_parallels=(49, 77))
-
-    # initialize figure (108*1500/2700=60)
-    fig, grid = apl.subplots_mm(
-        ncols=2, nrows=1, sharex=True, sharey=True, figsize=(192, 108),
-        subplot_kw=dict(projection=proj), gridspec_kw=dict(
-            left=0, right=192-120, bottom=0, top=0, wspace=0))
-    tsax = fig.add_axes_mm([120+10, 30, 192-120-20, 108-40])
-    cax = fig.add_axes_mm([120+10, 15, 192-120-20, 5])
-
-    # prepare map axes
-    for i, ax in enumerate(grid):
-        ax.set_extent([-2500e3, -1000e3, 150e3, 2850e3], crs=ax.projection)
-        ax.outline_patch.set_ec('none')
-
-    # add delimitors on last axes
-    for i in range(2):
-        ax.plot([i, i], [0, 1], color='k', clip_on=False,
-                transform=ax.transAxes, zorder=3)
-
-    return fig, grid, cax, tsax
-
-
-# Map elements
-# ------------
-
-def draw_natural_earth(ax=None, mode='gs', **kwargs):
-    """Add Natural Earth geographic data vectors."""
-    ax = ax or plt.gca()
-    edgecolor = '#0978ab' if mode == 'co' else '0.25'
-    facecolor = '#c6ecff' if mode == 'co' else '0.95'
-    cne.add_rivers(ax=ax, edgecolor=edgecolor, zorder=0, **kwargs)
-    cne.add_lakes(ax=ax, edgecolor=edgecolor, facecolor=facecolor, zorder=0,
-                  **kwargs)
-    cne.add_coastline(ax=ax, edgecolor=edgecolor, zorder=0, **kwargs)
 
 
 # Figure saving
@@ -91,8 +46,25 @@ def save_animation_frame(func, outdir, t, *args, **kwargs):
 def draw(t):
     """Plot complete figure for given time."""
 
-    # initialize figure
-    fig, grid, cax, tsax = utils.subplots()
+    # initialize figure (108*1500/2700=60)
+    fig, grid = apl.subplots_mm(
+        ncols=2, nrows=1, sharex=True, sharey=True, figsize=(192, 108),
+        gridspec_kw=dict(left=0, right=192-120, bottom=0, top=0, wspace=0),
+        subplot_kw=dict(projection=ccrs.LambertConformal(
+            central_longitude=-95, central_latitude=49,
+            standard_parallels=(49, 77))))
+    tsax = fig.add_axes_mm([120+10, 30, 192-120-20, 108-40])
+    cax = fig.add_axes_mm([120+10, 15, 192-120-20, 5])
+
+    # prepare map axes
+    for i, ax in enumerate(grid):
+        ax.set_extent([-2500e3, -1000e3, 150e3, 2850e3], crs=ax.projection)
+        ax.spines['geo'].set_ec('none')
+
+    # add delimitors on last axes
+    for i in range(2):
+        ax.plot([i, i], [0, 1], color='k', clip_on=False,
+                transform=ax.transAxes, zorder=3)
     twax = tsax.twiny()
 
     # contour levels
@@ -131,7 +103,10 @@ def draw(t):
                     label=r'surface velocity ($m\,a^{-1}$)'))
 
         # add map elements
-        utils.draw_natural_earth(ax, scale='50m')
+        cne.add_rivers(ax=ax, edgecolor='0.25', zorder=0, scale='50m')
+        cne.add_lakes(ax=ax, edgecolor='0.25', facecolor='0.95', zorder=0,
+                      scale='50m')
+        cne.add_coastline(ax=ax, edgecolor='0.25', zorder=0, scale='50m')
         cde.add_subfig_label(rec, ax=ax, loc='nw')
         cde.add_subfig_label('{:.1f} ka'.format(-t/1e3), ax=ax, loc='ne')
 
@@ -171,10 +146,10 @@ def main():
     """Main program for command-line execution."""
 
     # start and end of animation
-    t0, t1, dt = -120000, -0, 100
+    t0, t1, dt = -120000, -0, 10000
 
     # output frame directories
-    outdir = os.path.join(os.environ['HOME'], 'anim', 'anim_cordillera_cycle')
+    outdir = os.path.join(os.environ['HOME'], 'anim', 'anim_cordillera_dual')
 
     # iterable arguments to save animation frames
     iter_args = [(draw, outdir, t) for t in range(t0+dt, t1+1, dt)]
@@ -185,7 +160,7 @@ def main():
 
     # plot all frames in parallel
     with mp.Pool(processes=4) as pool:
-        pool.starmap(utils.save_animation_frame, iter_args)
+        pool.starmap(save_animation_frame, iter_args)
 
 
 if __name__ == '__main__':
