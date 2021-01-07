7b3a463b4af0ac559e490e7537aff1f8a6a524f6
Julien Seguinot <juseg@posteo.eu>
2021-01-07 08:52:39 +0100

    Lint animation code with Pylint (except FIXMEs).

--- movies/anim_cordillera_dual.py
+++ movies/anim_cordillera_dual.py
@@ -26,16 +26,16 @@ plt.rc('axes', prop_cycle=plt.cycler(color=plt.get_cmap('Paired').colors))
 # Figure saving
 # -------------
 
-def save_animation_frame(func, outdir, t, *args, **kwargs):
+def save_animation_frame(func, outdir, time, *args, **kwargs):
     """Save figure produced by func as animation frame if missing."""
 
     # check if file exists
-    fname = os.path.join(outdir, '{:06d}.png').format(t+120000)
+    fname = os.path.join(outdir, '{:06d}.png').format(time+120000)
     if not os.path.isfile(fname):
 
         # assemble figure and save
         print('plotting {:s} ...'.format(fname))
-        fig = func(t, *args, **kwargs)
+        fig = func(time, *args, **kwargs)
         fig.savefig(fname)
         plt.close(fig)
 
@@ -43,7 +43,7 @@ def save_animation_frame(func, outdir, t, *args, **kwargs):
 # Main visual
 # -----------
 
-def draw(t):
+def draw(time):
     """Plot complete figure for given time."""
 
     # initialize figure (108*1500/2700=60)
@@ -53,24 +53,16 @@ def draw(t):
         subplot_kw=dict(projection=ccrs.LambertConformal(
             central_longitude=-95, central_latitude=49,
             standard_parallels=(49, 77))))
-    tsax = fig.add_axes_mm([120+10, 30, 192-120-20, 108-40])
     cax = fig.add_axes_mm([120+10, 15, 192-120-20, 5])
+    tsax = fig.add_axes_mm([120+10, 30, 192-120-20, 108-40])
+    twax = tsax.twiny()
 
     # prepare map axes
     for i, ax in enumerate(grid):
         ax.set_extent([-2500e3, -1000e3, 150e3, 2850e3], crs=ax.projection)
         ax.spines['geo'].set_ec('none')
-
-    # add delimitors on last axes
-    for i in range(2):
-        ax.plot([i, i], [0, 1], color='k', clip_on=False,
-                transform=ax.transAxes, zorder=3)
-    twax = tsax.twiny()
-
-    # contour levels
-    levels = range(0, 5000, 200)
-    majors = [lev for lev in levels if lev % 1000 == 0]
-    minors = [lev for lev in levels if lev % 1000 != 0]
+        fig.lines.append(plt.Line2D(
+            [1, 1], [0, 1], transform=ax.transAxes))
 
     # for each record
     for i, rec in enumerate(['GRIP', 'EPICA']):
@@ -85,15 +77,17 @@ def draw(t):
         # FIXME port alps util open_subdataset
         # FIXME port alps util shaded_relief
         with pismx.open.mfdataset(rundir+'ex.???????.nc') as ds:
-            ds = ds.sel(age=-t/1e3)
+            ds = ds.sel(age=-time/1e3)
             ds = ds.transpose(..., 'x')  # FIXME in pismx?
             ds.topg.plot.imshow(
                 ax=ax, cmap='Greys', vmin=0e3, vmax=3e3, add_colorbar=False,
                 zorder=-1)
             ds.usurf.where(ds.thk >= 1.0).plot.contour(
-                ax=ax, colors=['0.25'], levels=majors, linewidths=0.25)
+                levels=[lev for lev in range(0, 5000, 200) if lev % 1000 == 0],
+                ax=ax, colors=['0.25'], linewidths=0.25)
             ds.usurf.where(ds.thk >= 1.0).plot.contour(
-                ax=ax, colors=['0.25'], levels=minors, linewidths=0.1)
+                levels=[lev for lev in range(0, 5000, 200) if lev % 1000 != 0],
+                ax=ax, colors=['0.25'], linewidths=0.1)
             ds.thk.plot.contour(
                 ax=ax, colors=['0.25'], levels=[1.0], linewidths=0.25)
             ds.velsurf_mag.where(ds.thk >= 1.0).plot.imshow(
@@ -108,21 +102,23 @@ def draw(t):
                       scale='50m')
         cne.add_coastline(ax=ax, edgecolor='0.25', zorder=0, scale='50m')
         cde.add_subfig_label(rec, ax=ax, loc='nw')
-        cde.add_subfig_label('{:.1f} ka'.format(-t/1e3), ax=ax, loc='ne')
+        cde.add_subfig_label('{:.1f} ka'.format(-time/1e3), ax=ax, loc='ne')
 
         # plot temperature forcing
         with pismx.open.dataset('~/pism/input/dt/'+dtfile+'.nc') as ds:
-            data = ds.delta_T[ds.time <= t]
+            data = ds.delta_T[ds.time <= time]
             tsax.plot(data, data.age, color=color, alpha=0.25)
-            tsax.text(data[-1], -t/1e3, '  {:.1f}°C'.format(float(data[-1])),
-                      ha='left', va='center', clip_on=True, color=color)
+            tsax.text(
+                data[-1], -time/1e3, '  {:.1f}°C'.format(float(data[-1])),
+                ha='left', va='center', clip_on=True, color=color)
 
         # plot ice volume time series
         with pismx.open.mfdataset(rundir+'/ts.???????.nc') as ds:
-            data = ds.slvol[ds.age >= -t/1e3]
+            data = ds.slvol[ds.age >= -time/1e3]
             twax.plot(data, data.age, color=color)
-            twax.text(data[-1], -t/1e3, '  {:.1f} m'.format(float(data[-1])),
-                      ha='left', va='center', clip_on=True, color=color)
+            twax.text(
+                data[-1], -time/1e3, '  {:.1f} m'.format(float(data[-1])),
+                ha='left', va='center', clip_on=True, color=color)
 
     # set time series axes properties
     tsax.set_ylim(120.0, 0.0)
@@ -146,13 +142,13 @@ def main():
     """Main program for command-line execution."""
 
     # start and end of animation
-    t0, t1, dt = -120000, -0, 10000
+    start, end, step = -120000, -0, 10000
 
     # output frame directories
     outdir = os.path.join(os.environ['HOME'], 'anim', 'anim_cordillera_dual')
 
     # iterable arguments to save animation frames
-    iter_args = [(draw, outdir, t) for t in range(t0+dt, t1+1, dt)]
+    iter_args = [(draw, outdir, t) for t in range(start+step, end+1, step)]
 
     # create frame directory if missing
     if not os.path.isdir(outdir):
