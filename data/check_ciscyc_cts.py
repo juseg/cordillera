ac4f188e71b1c63f54fc0b42b9848298cd9cec5d
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-04-05 11:07:10 +0200

    Verify it produces consistent files.

--- data/check_ciscyc_cts.py
+++ data/check_ciscyc_cts.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2023, Julien Seguinot (juseg.dev)
+# Copyright (c) 2023-2024, Julien Seguinot (juseg.dev)
 # Creative Commons Attribution-ShareAlike 4.0 International License
 # (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
@@ -21,17 +21,18 @@ def main():
     for newfile in sorted(glob.glob('ciscyc.*km.*.??.*a.nc')):
 
         # download corresponding online file (use old 1ka for new 100a files)
-        oldfile = newfile[:-3].replace('100a', '1ka') + '.old.nc'
+        webfile = newfile.replace('100a', '1ka')
+        oldfile = webfile[:-3] + '.old.nc'
         if not os.path.isfile(oldfile):
             urllib.request.urlretrieve(
-                'https://zenodo.org/record/3606536/files/' + newfile, oldfile)
+                'https://zenodo.org/record/3606536/files/' + webfile, oldfile)
 
         # open old and new datasets
         with hyoga.open.dataset(newfile, chunks=None) as new, \
                 hyoga.open.dataset(oldfile, chunks=None) as old:
 
             # compute the difference
-            diff = (new - old)
+            diff = new - old
             std = (diff**2).mean()
             diff_vars = [var for var in std if std[var] != 0]
             if len(diff_vars) > 0:
